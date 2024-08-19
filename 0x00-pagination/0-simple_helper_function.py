e70a1404ce551e4a6b55862a41567aed63906a42
n-elmasry <almassrynada411@gmail.com>
2024-08-19 20:18:28 +0500

    task0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""index_range"""
+
+import requests
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
