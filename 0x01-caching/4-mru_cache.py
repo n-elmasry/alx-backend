f80e0a1c5a3aef29736e9ef9616ed8de8aaa72ea
n-elmasry <almassrynada411@gmail.com>
2024-08-22 01:09:34 +0500

    task4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
-"""MRUCache"""
+"""Most Recently Used caching module.
+"""
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
     """inherits from BaseCaching and is a caching system"""
+
+    def __init__(self):
+        """init"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key key."""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            most_rused, _ = self.cache_data.popitem(last=True)
+            print(f'DISCARD: {most_rused}')
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ return the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        item = self.cache_data.pop(key)
+        self.cache_data[key] = item
+
+        return item
