1015d868ccfa0965121950502ef1029770b58373
n-elmasry <almassrynada411@gmail.com>
2024-08-27 23:02:09 +0500

    task 3

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
-""""""
+"""flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, request
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """index"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
